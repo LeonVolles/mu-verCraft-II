c5de9e2e22e05c7f5cd462f14587cd027a449624
Leon Volles <LeonVolles@users.noreply.github.com>
2025-10-13 15:25:36 +0200

    refactor project structure

---
+++ 3_Software/Firmware/src/variables.h
@@ -0,0 +1,20 @@
+// hier globale var definieren, auf die der gesamte Code zugreift
+
+// Flags, Mutex
+
+// Wifi SSID, PW, IP Adressen
+
+// IO Pins
+
+// Contrl loop constants
+// T = 
+// p;i;d;
+//filter??
+
+// Dimentions
+// sensor positions
+// motor positions
+
+// battery limits
+
+// 
