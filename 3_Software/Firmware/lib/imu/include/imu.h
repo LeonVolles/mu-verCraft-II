1689db2000c6072ecad539d88cd7a87ef2855f31
LuisGbs <l.goebes1@gmail.com>
2025-10-22 19:37:27 +0200

    raw prep for imu coding

--- 3_Software/Firmware/lib/imu/include/imu.h
+++ 3_Software/Firmware/lib/imu/include/imu.h
@@ -1,44 +1,52 @@
-// #ifndef IMU_H
-// #define IMU_H
+#ifndef IMU_H
+#define IMU_H
 
-// #include <Arduino.h>
+#include <Arduino.h>
 
-// class IMU {
-// private:
-//     float accelX, accelY, accelZ;
-//     float gyroX, gyroY, gyroZ;
-//     float roll, pitch, yaw;
-    
-// public:
-//     IMU();
-    
-//     // Initialization
-//     void init();
-    
-//     // Update function with delta time
-//     void update(float dt_s);
-    
-//     // Accelerometer data
-//     void setAccel(float ax, float ay, float az);
-//     void getAccel(float* ax, float* ay, float* az);
-    
-//     // Gyroscope data
-//     void setGyro(float gx, float gy, float gz);
-//     void getGyro(float* gx, float* gy, float* gz);
-    
-//     // Angle data (Euler angles)
-//     void setAngles(float roll, float pitch, float yaw);
-//     void getAngles(float* roll, float* pitch, float* yaw);
-    
-//     // Individual angle setters
-//     void setAngleRoll(float r);
-//     void setAnglePitch(float p);
-//     void setAngleYaw(float y);
-    
-//     // Individual angle getters
-//     float getRoll();
-//     float getPitch();
-//     float getYaw();
-// };
+class IMU
+{
+private:
+    float accelX, accelY, accelZ;
+    float gyroX, gyroY, gyroZ;
+    float roll, pitch, yaw;
 
-// #endif // IMU_H
+public:
+    // Constructor
+    IMU();
+
+    // Initialization
+    void init();
+
+    // Reset global IMU coordinate system
+    void resetGlobalCoordSystem();
+
+    // Update function with delta time
+    void update(float dt_s);
+
+    // ***Gyroscope data***
+    void getGyro(float *gx, float *gy, float *gz);
+
+    // Angle data (Euler angles)
+    void setAngles(float roll, float pitch, float yaw);
+    void getAngles(float *roll, float *pitch, float *yaw);
+
+    // Individual angle getters
+    float getAngleRoll();
+    float getAnglePitch();
+    float getAngleYaw();
+
+    // ***Accelerometer data***
+    // Accelerations x,y,z
+    void getAccel(float *ax, float *ay, float *az);
+
+    // Position data x,y,z
+    void getPosition(float *x, float *y, float *z);
+    void setPosition(float x, float y, float z);
+
+    // Individual position setters
+    void setPositionX(float x);
+    void setPositionY(float y);
+    void setPositionZ(float z);
+};
+
+#endif // IMU_H
--- 3_Software/Firmware/lib/imu/src/imu.cpp
+++ 3_Software/Firmware/lib/imu/src/imu.cpp
@@ -1,64 +1,8 @@
-// #include "imu.h"
+#include "imu.h"
 
-// IMU::IMU() {
-//     // TODO: Initialize member variables
-// }
+IMU::IMU()
+{
+    // TODO: Initialize member variables
+}
 
-// void IMU::init() {
-//     // TODO: Initialize IMU sensor
-// }
-
-// void IMU::update(float dt_s) {
-//     // TODO: Update IMU readings
-// }
-
-// void IMU::setAccel(float ax, float ay, float az) {
-//     // TODO: Set accelerometer values
-// }
-
-// void IMU::getAccel(float* ax, float* ay, float* az) {
-//     // TODO: Get accelerometer values
-// }
-
-// void IMU::setGyro(float gx, float gy, float gz) {
-//     // TODO: Set gyroscope values
-// }
-
-// void IMU::getGyro(float* gx, float* gy, float* gz) {
-//     // TODO: Get gyroscope values
-// }
-
-// void IMU::setAngles(float roll, float pitch, float yaw) {
-//     // TODO: Set angle values
-// }
-
-// void IMU::getAngles(float* roll, float* pitch, float* yaw) {
-//     // TODO: Get angle values
-// }
-
-// void IMU::setAngleRoll(float r) {
-//     // TODO: Set roll angle
-// }
-
-// void IMU::setAnglePitch(float p) {
-//     // TODO: Set pitch angle
-// }
-
-// void IMU::setAngleYaw(float y) {
-//     // TODO: Set yaw angle
-// }
-
-// float IMU::getRoll() {
-//     // TODO: Return roll angle
-//     return 0.0;
-// }
-
-// float IMU::getPitch() {
-//     // TODO: Return pitch angle
-//     return 0.0;
-// }
-
-// float IMU::getYaw() {
-//     // TODO: Return yaw angle
-//     return 0.0;
-// }
+// ToDo ...
